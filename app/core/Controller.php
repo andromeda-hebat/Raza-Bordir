70fe4eee94ba2bec01f960147b9dac6db2d4bd5d
Farrel Augusta Dinata <140877757+FarrelAD@users.noreply.github.com>
2024-11-14 09:06:46 +0000

    add Controller.php for the core class of controller

---
+++ app/core/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+class Controller {
+    public ?Model model;
+
+    public function __construct() {
+
+    }
+
+    public function getModel(): Model {
+        return $this->model;
+    }
+
+    public function view(string $view, array $data = []): void {
+        require __DIR__ . '/../views/' . $view . '.php';
+    }
+}
