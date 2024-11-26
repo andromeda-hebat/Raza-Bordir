982ef0b66a5fa8434d5cc73c828e890eba80edd1
Stevan Zaky <stevanzaky87@gmail.com>
2024-11-26 17:02:17 +0700

    membuat halaman index

---
+++ app/views/components/navbar.php
@@ -0,0 +1,32 @@
+<nav class=" navbar d-flex align-items-center py-1 px-4 fixed-top" style="background-color: #FEFFD2;">
+    <!-- div untuk kiri -->
+    <div class="d-flex align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="size-6 me-1" width="30" height="30">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
+        </svg>
+        <a class="text-decoration-none text-black ms-1">
+            Menu
+        </a>
+    </div>
+    <!-- div untuk tengah -->
+    <div class="align-items-center">
+        <p class="p-0 m-0 fs-2" style="font-family: 'Italianno', serif;">Raza Bordir</p>
+    </div>
+    <!-- div untuk kanan -->
+    <div class="d-flex align-items-center">
+        <a href="#" class="text-decoration-none text-black me-1">
+            Beranda
+        </a>
+        <a href="#" class="text-decoration-none text-black mx-1">
+            Produk
+        </a>
+        <a href="#" class="text-decoration-none text-black mx-1">
+            Promo
+        </a>
+        <a href="#" class="text-decoration-none text-black ms-1">
+            Tentang
+        </a>
+    </div>
+</nav>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/reddit-sans@5.1.0/index.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet">
 </head>
 
 <body>
