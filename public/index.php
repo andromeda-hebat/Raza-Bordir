ed7f716ccb1379763fbc5d750551c868ae5a3db0
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-03 21:13:08 +0700

    remove config folder and change to .env file configuration

--- public/index.php
+++ public/index.php
@@ -4,5 +4,10 @@ require_once __DIR__ . '/../app/routes/web.php';
 require_once __DIR__ . '/../app/core/Router.php';
 
 use App\Core\Router;
+use Dotenv\Dotenv;
 
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+session_start();
 Router::run();
