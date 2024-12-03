9cda9f705a3993b1e4eb281b43349be68f2d2eb5
Stevan Zaky <stevanzaky87@gmail.com>
2024-12-03 22:36:55 +0700

    membuat navbar dan dashbord menjadi lebih responsif

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -1,32 +1,44 @@
-<nav class=" navbar d-flex align-items-center py-1 px-4 fixed-top" style="background-color: #FEFFD2;">
-    <!-- div untuk kiri -->
-    <div class="d-flex align-items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="size-6 me-1" width="30" height="30">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
-        </svg>
-        <a class="text-decoration-none text-black ms-1">
-            Menu
-        </a>
-    </div>
-    <!-- div untuk tengah -->
-    <div class="align-items-center">
-        <p class="p-0 m-0 fs-2" style="font-family: 'Italianno', serif;">Raza Bordir</p>
-    </div>
-    <!-- div untuk kanan -->
-    <div class="d-flex align-items-center">
-        <a href="#" class="text-decoration-none text-black me-1">
-            Beranda
-        </a>
-        <a href="#" class="text-decoration-none text-black mx-1">
-            Produk
-        </a>
-        <a href="#" class="text-decoration-none text-black mx-1">
-            Promo
-        </a>
-        <a href="#" class="text-decoration-none text-black ms-1">
-            Tentang
-        </a>
-    </div>
+<nav class=" navbar navbar-expand-lg d-flex align-items-between py-1 px-2 fixed-top" style="background-color: #FEFFD2;">
+
+    <div class="container-fluid">
+        <!-- div untuk kiri -->
+        <div class="d-flex align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-6 me-1" width="30" height="30">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
+            </svg>
+            <a class="text-decoration-none text-black ms-1">
+                Menu
+            </a>
+        </div>
+        <!-- div untuk tengah -->
+        <div class="d-flex justify-content-center">
+            <p class="p-0 m-0 fs-2 text-center" style="font-family: 'Italianno', serif;">Raza Bordir</p>
+        </div>
+
+        <!-- div untuk kanan -->
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end flex-grow-1" id="navbarNav" style="max-width:55vh;">
+                <div>
+                    <a href="#" class="text-decoration-none text-black me-1">
+                        Beranda
+                    </a>
+                    <a href="#" class="text-decoration-none text-black mx-1">
+                        Produk
+                    </a>
+                    <a href="#" class="text-decoration-none text-black mx-1">
+                        Promo
+                    </a>
+                    <a href="#" class="text-decoration-none text-black ms-1">
+                        Tentang
+                    </a>
+
+                </div>
+            </div>
+        
 </nav>
--- app/views/index.php
+++ app/views/index.php
@@ -1,15 +1,17 @@
+
+
 <?php include __DIR__ . '/components/navbar.php' ?>
 <main>
+    <!-- Halaman 1 -->
     <section class="vh-100 d-flex align-items-end"
-        style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('assets/img/mesin-bordir.jpg');  background-size: cover; background-repeat: no-repeat; background-position: center;">
-
-        <div class="m-5" style="color: #FEFFD2;">
-            <h1>
-                A Masterpiece In Stitches
+        style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('assets/img/mesin-bordir.jpg'); background-size: cover; background-repeat: no-repeat; background-position: center;">
+        <div class="container text-md-start m-5" style="color: #FEFFD2;">
+            <h1 class="display-6 fw-bold" style="font-family: 'Alegreya Sans SC';">
+                Sebuah Mahakarya Dalam Jahitan
             </h1>
-            <h5>
-                Experience the beauty of perfect stitches,<br>
-                Computer embroidery brings designs to life with stunning precision.
+            <h5 class="mt-3" style="font-family: 'Inter';">
+                Rasakan keindahan jahitan yang sempurna,<br class="d-none d-md-block">
+                Bordir komputer menghidupkan desain dengan presisi yang menakjubkan.
             </h5>
         </div>
     </section>
