b71ea6b17c6271a90811139d7de643a10a00c304
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-03 23:08:47 +0700

    add setup content for ulasan page

--- app/views/pages/admin/ulasan.php
+++ app/views/pages/admin/ulasan.php
@@ -3,5 +3,26 @@
     
     <main>
         <h1>Ulasan</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID Pemesan</th>
+                    <th>Ulasan</th>
+                    <th>Tanggal</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>P0001</td>
+                    <td>Bordirnya bagus!</td>
+                    <td>12/07/2024</td>
+                    <td>
+                        <button>Hapus</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 </div>
