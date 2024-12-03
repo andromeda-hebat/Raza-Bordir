488bb1e153c78fa7b448bfa89682a0a488d35f00
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-03 22:55:20 +0700

    add setup content for kelola katalog produk page

--- app/views/pages/admin/kelola_katalog_produk.php
+++ app/views/pages/admin/kelola_katalog_produk.php
@@ -1,7 +1,30 @@
 <div class="d-flex">
     <?php include __DIR__ . '/../../components/admin/sidebar.php' ?>
     
-    <main>
+    <main class="w-100">
         <h1>Kelola Katalog Produk</h1>
+
+        <a href="/katalog-produk/tambah">Tambah</a>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Nama Produk</th>
+                    <th>Deskripsi Produk</th>
+                    <th>Foto Produk</th>
+                    <th>Kategori</th>
+                    <th>Harga</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Logo Sekolah</td>
+                    <td>Ini adalah sebuah logo sekolah</td>
+                    <td>logo_sekolah.jpg</td>
+                    <td>Bordir logo</td>
+                    <td>Rp 5.000/pcs</td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 </div>
