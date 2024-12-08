dde36b8f5707793104cdd38d1e8f04a8e36701ef
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-09 05:58:56 +0700

    make responsive navbar for desktop screen

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -2,7 +2,8 @@
 
     <div class="container-fluid">
         <div class="d-md-none">
-            <button type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileNav" aria-controls="mobileNav" style="background-color: inherit; border: none;">
+            <button type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileNav" aria-controls="mobileNav"
+                style="background-color: inherit; border: none;">
                 ☰
             </button>
         </div>
@@ -11,23 +12,41 @@
             <p class="p-0 m-0 fs-2 text-center" style="font-family: 'Italianno', serif;">Raza Bordir</p>
         </div>
 
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="mobileNav" aria-labelledby="mobileNavLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="mobileNavLabel">Menu</h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
-                    aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body p-0">
-                <ul class="list-unstyled p-4">
-                    <li><a href="#" class="nav-link">Beranda</a></li>
-                    <li><a href="#" class="nav-link">Produk</a></li>
-                    <li><a href="#" class="nav-link">Promo</a></li>
-                    <li><a href="#" class="nav-link">Tentang</a></li>
-                </ul>
-                <div class="p-4 w-100 position-absolute bottom-0" style="background-color: #EAE1D0; height: 100px">
-                    <a href="#" class="text-decoration-none text-black">Hubungi Kami</a>
-                </div>
-            </div>
+        <!-- Desktop Links -->
+        <div class="collapse navbar-collapse d-none d-md-flex justify-content-end">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Produk</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Promo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tentang</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
+
+<!-- Sliding offcanvas for mobile -->
+<div class="offcanvas offcanvas-start" tabindex="-1" id="mobileNav" aria-labelledby="mobileNavLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="mobileNavLabel">Menu</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body p-0">
+        <ul class="list-unstyled p-4">
+            <li><a href="#" class="nav-link">Beranda</a></li>
+            <li><a href="#" class="nav-link">Produk</a></li>
+            <li><a href="#" class="nav-link">Promo</a></li>
+            <li><a href="#" class="nav-link">Tentang</a></li>
+        </ul>
+        <div class="p-4 w-100 position-absolute bottom-0" style="background-color: #EAE1D0; height: 100px">
+            <a href="#" class="text-decoration-none text-black">Hubungi Kami</a>
+        </div>
+    </div>
+</div>
