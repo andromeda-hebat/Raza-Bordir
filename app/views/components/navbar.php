847b5108cfbd605a89ce8caaac051e689856b4fe
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-09 01:18:56 +0700

    fix navbar

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -1,44 +1,29 @@
 <nav class=" navbar navbar-expand-lg d-flex align-items-between py-1 px-2 fixed-top" style="background-color: #FEFFD2;">
 
     <div class="container-fluid">
-        <!-- div untuk kiri -->
-        <div class="d-flex align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-6 me-1" width="30" height="30">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
-            </svg>
-            <a class="text-decoration-none text-black ms-1">
-                Menu
-            </a>
+        <div class="d-md-none">
+            <button type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileNav" aria-controls="mobileNav" style="background-color: inherit; border: none;">
+                ☰
+            </button>
         </div>
-        <!-- div untuk tengah -->
-        <div class="d-flex justify-content-center">
+
+        <div class="position-absolute start-50 translate-middle-x">
             <p class="p-0 m-0 fs-2 text-center" style="font-family: 'Italianno', serif;">Raza Bordir</p>
         </div>
 
-        <!-- div untuk kanan -->
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end flex-grow-1" id="navbarNav" style="max-width:55vh;">
-                <div>
-                    <a href="#" class="text-decoration-none text-black me-1">
-                        Beranda
-                    </a>
-                    <a href="#" class="text-decoration-none text-black mx-1">
-                        Produk
-                    </a>
-                    <a href="#" class="text-decoration-none text-black mx-1">
-                        Promo
-                    </a>
-                    <a href="#" class="text-decoration-none text-black ms-1">
-                        Tentang
-                    </a>
-
-                </div>
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="mobileNav" aria-labelledby="mobileNavLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="mobileNavLabel">Menu</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                    aria-label="Close"></button>
             </div>
-        
+            <div class="offcanvas-body">
+                <ul class="list-unstyled">
+                    <li><a href="#" class="nav-link">Beranda</a></li>
+                    <li><a href="#" class="nav-link">Produk</a></li>
+                    <li><a href="#" class="nav-link">Promo</a></li>
+                    <li><a href="#" class="nav-link">Tentang</a></li>
+                </ul>
+            </div>
+        </div>
 </nav>
