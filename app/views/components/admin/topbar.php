b2d8b6e6625b1a69d180f42085c866f9d11a899a
Stevan Zaky <stevanzaky87@gmail.com>
2024-12-09 22:47:49 +0700

    merapikan tampilan admin

---
+++ app/views/components/admin/topbar.php
@@ -0,0 +1,14 @@
+<div id="topbar" class="navbar d-flex justify-content-between pt-3 pb-3" style="height: 10vh; background-color: #FFFF ;">
+    <div class="ps-5">
+        <div class="">
+            <svg width="25" height="25" viewBox="0 0 41 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="41" height="44" fill="#D9D9D9" />
+            </svg>
+
+        </div>
+    </div>
+    <div class="pe-5 d-flex align-items-center">
+        <h6 class="my-0 me-3">Admin</h6>
+        <img src="/assets/img/admin.jpg" alt="" style="width: 5vh;">
+    </div>
+</div>
--- app/views/pages/admin/dashboard.php
+++ app/views/pages/admin/dashboard.php
@@ -1,7 +1,12 @@
 <div class="d-flex">
     <?php include __DIR__ . '/../../components/admin/sidebar.php' ?>
+    <div class="position-top w-100">
+        <?php include __DIR__ . '/../../components/admin/topbar.php' ?>
+        <main style="height: 90vh;">
+            <div class="p-5">
+                <h1>INI NANTI DASHBOARD JIKA ADA</h1>
+            </div>
 
-    <main>
-        <h1>Dashboard</h1>
-    </main>
+        </main>
+    </div>
 </div>
