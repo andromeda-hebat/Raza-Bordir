02cc2e14d71d712091750d9d3c42a0771bf98dc0
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-10 10:03:22 +0700

    refactor: move auth logic from admin to auth

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -6,29 +6,6 @@ use App\Core\Controller;
 
 class AdminController extends Controller
 {
-    public function showLoginPage(): void
-    {
-        $this->view("templates/header", [
-            'title'=>"Admin Login"
-        ]);
-        $this->view("pages/admin/login");
-        $this->view("templates/footer");
-    }
-
-    public function processLogin(): void
-    {
-        if (isset($_POST['username']) && isset($_POST['password'])) {
-            if ($_POST['username'] == "Raza" && $_POST['password'] == "bordir") {
-                $_SESSION['username'] = $_POST['username'];
-                header("Location: /dashboard");
-                return;
-            } else {
-                echo "Wrong username and password!";
-                return;
-            }
-        }
-    }
-
     public function showDashboard(): void
     {
         $this->view("templates/header", [
--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -6,6 +6,29 @@ use App\Core\Controller;
 
 class AuthController extends Controller
 {
+    public function showLoginPage(): void
+    {
+        $this->view("templates/header", [
+            'title'=>"Admin Login"
+        ]);
+        $this->view("pages/admin/login");
+        $this->view("templates/footer");
+    }
+
+    public function processLogin(): void
+    {
+        if (isset($_POST['username']) && isset($_POST['password'])) {
+            if ($_POST['username'] == "Raza" && $_POST['password'] == "bordir") {
+                $_SESSION['username'] = $_POST['username'];
+                header("Location: /dashboard");
+                return;
+            } else {
+                echo "Wrong username and password!";
+                return;
+            }
+        }
+    }
+    
     public function sendPageMethodNotAllowed(): void
     {
         http_response_code(405);
