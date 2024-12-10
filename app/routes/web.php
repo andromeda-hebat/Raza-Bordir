90f9724278b202e6f8492f0ed669dee436109f59
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-10 10:56:02 +0700

    fix routing for user admin authentication

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use App\Core\Router;
-use App\Controllers\{CustomerController, AdminController};
+use App\Controllers\{CustomerController, AuthController, AdminController};
 
 
 Router::add('GET', '/', CustomerController::class, 'index');
@@ -18,10 +18,11 @@ Router::add('GET', '/kontak', CustomerController::class, 'showContact');
 
 
 // Admin
-Router::add('GET', '/admin', AdminController::class, 'showLoginPage');
-Router::add('POST', '/admin', AdminController::class, 'processLogin');
+Router::add('GET', '/admin', AuthController::class, 'showLoginPage');
+Router::add('POST', '/admin', AuthController::class, 'processLogin');
 Router::add('GET', '/dashboard', AdminController::class, 'showDashboard');
 Router::add('GET', '/kelola-pesanan', AdminController::class, 'showKelolaPesananPage');
 Router::add('GET', '/katalog-produk', AdminController::class, 'showKelolaKatalogProduk');
-Router::add('GET', '/ulasan', AdminController::class, 'showUlasan');
 Router::add('GET', '/katalog-produk/tambah', AdminController::class, 'showTambahProduk');
+Router::add('GET', '/ulasan', AdminController::class, 'showUlasan');
+Router::add('GET', '/penjualan', AdminController::class, 'manageSales');
