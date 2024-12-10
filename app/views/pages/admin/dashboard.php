00a24177dedd4d9d9742ee7cb7a5fb8f604bfd2e
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-10 09:51:18 +0700

    add dashboard content for admin panel

--- app/views/pages/admin/dashboard.php
+++ app/views/pages/admin/dashboard.php
@@ -4,7 +4,25 @@
         <?php include __DIR__ . '/../../components/admin/topbar.php' ?>
         <main style="height: 90vh;">
             <div class="p-5">
-                <h1>INI NANTI DASHBOARD JIKA ADA</h1>
+                <div>
+                    <h1>Kategori Produk Terlaris</h1>
+                    <div class="border border-black w-100" style="height: 100px"></div>
+                </div>
+
+                <div class="d-flex">
+                    <div class="w-50 me-3">
+                        <h2>Jumlah Pesanan</h2>
+                        <div class="border border-black w-100" style="height: 100px"></div>
+                    </div>
+                    <div class="w-50 ms-3">
+                        <h2>Jumlah Pengunjung</h2>
+                        <div class="border border-black w-100" style="height: 100px"></div>
+                    </div>
+                </div>
+                <div>
+                    <h2>Grafik Penjualan</h2>
+                    <div class="border border-black w-100" style="height: 400px"></div>
+                </div>
             </div>
 
         </main>
