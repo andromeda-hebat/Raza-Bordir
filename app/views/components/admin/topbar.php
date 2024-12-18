ff3383296debb9d6c5b5cdbd495bae51c3fcbb15
Stevan Zaky <stevanzaky87@gmail.com>
2024-12-18 19:01:12 +0700

    merapikan

--- app/views/components/admin/topbar.php
+++ app/views/components/admin/topbar.php
@@ -3,7 +3,7 @@
 function Topbar(): string
 {
     return <<<HTML
-    <div id="topbar" class="navbar d-flex justify-content-between pt-3 pb-3 position-fixed" style="width: 81%; height: 10vh; background-color: #FFFF ;">
+    <div id="topbar" class="navbar d-flex justify-content-between pt-3 pb-3 position-fixed" style="width: 84%; height: 10vh; background-color: #FFFF ; z-index: 1000;">
         <div class="ps-3">
             <div class="">
                 <svg width="25" height="25" viewBox="0 0 41 44" fill="none" xmlns="http://www.w3.org/2000/svg">
