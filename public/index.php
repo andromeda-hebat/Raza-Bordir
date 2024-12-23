6ffb5e5e10022d4a2672cd77c6725a4b95ec3b8e
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-23 10:21:15 +0700

    add: more proper session management

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__ . '/../app/routes/web.php';
-require_once __DIR__ . '/../app/core/Router.php';
 
 use App\Core\Router;
 use Dotenv\Dotenv;
@@ -9,7 +8,13 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
+session_set_cookie_params([
+    'httponly' => true,
+    'samesite' => 'Strict',
+    'lifetime' => 3600,
+]);
 session_start();
+session_regenerate_id(true);
 
 Router::initialize();
 Router::run();
