b860f9c4045a86d7d80ffbe619a5de8d6d13c483
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-23 16:05:56 +0700

    add: setup database logic to insert new order

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Helper\FileManager;
+use App\Repository\OrderRepository;
 
 class CustomerController extends Controller
 {
@@ -35,6 +37,41 @@ class CustomerController extends Controller
 
     public function processCustomerOrder(): void
     {
+        if (
+            !isset($_POST['name']) || !isset($_POST['phone']) ||
+            !isset($_POST['address']) || !isset($_POST['amount']) ||
+            !isset($_POST['price']) || !isset($_POST['media']) ||
+            !isset($_FILES['design']) || !isset($_POST['note'])
+        ) {
+            $this->sendWarningJSON(400, "Data tidak komplit!");
+            exit;
+        }
+
+        $_FILES['design']['new_name'] = uniqid() . '--' . basename($_FILES['design']['name']);
+
+        $is_move_uploaded_file_success = FileManager::moveFile($_FILES, __DIR__ . '/../../storage/uploads/design_request/');
+
+        if (!$is_move_uploaded_file_success) {
+            $this->sendWarningJSON(500, "There is a warning while try to move uploaded file to the server!");
+            exit;
+        }
+
+        try {
+            OrderRepository::addNewOrder([
+                'name' => $_POST['name'],
+                'phone' => $_POST['phone'],
+                'address' => $_POST['address'],
+                'amount' => $_POST['amount'],
+                'price' => $_POST['price'],
+                'media' => $_POST['media'],
+                'design' => $_FILES['design']['name'],
+                'note' => $_POST['note']
+            ]);
+        } catch (\PDOException $e) {
+            $this->sendWarningJSON(500, "Database error!");
+            exit;
+        }
+
         echo json_encode([
             'message'=> "good bro!",
             'data' => $_POST,
