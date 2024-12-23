6aaf2b18c8dd6378d3ba93647ec761fdaefd7c1c
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-23 13:01:01 +0700

    put global css from header to separate css file

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -13,59 +13,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
-
-    <style>
-        * {
-            font-family: "Alegreya Sans SC", serif;
-        }
-
-        :root {
-            --ivory-cream-color: #EAE1D0;
-        }
-
-        main {
-            background-color: #FAF9F6;
-        }
-
-        .button {
-            background-color: #EAE1D0;
-            border: 1px solid black;
-            color: black;
-            box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-        }
-
-        .product-card {
-            aspect-ratio: 1/1;
-            box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-            max-height: 400px;
-            min-height: 150px;
-            width: 100%;
-            overflow: hidden;
-        }
-
-        @media (max-width: 768px) {
-            .product-card {
-                max-height: 300px;
-            }
-        }
-
-        @media (max-width: 576px) {
-            .product-card {
-                max-height: 250px;
-            }
-        }
-
-        .product-img {
-            object-position: center;
-            width: 100%;
-            height: auto;
-            max-width: 100%;
-        }
-
-        .price-tag {
-            font-size: 0.75rem;
-        }
-    </style>
+    <link rel="stylesheet" href="/assets/css/global.css">
 </head>
 
 <body>
