55876dce11734cd42c7b54aee9382f44314a9ca2
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-23 13:07:03 +0700

    add: some exception response in base controller

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,9 +2,36 @@
 
 namespace App\Core;
 
-abstract class Controller {
+class Controller {
 
-    public function view(string $view, array $data = []): void {
+    protected function view(string $view, array $data = []): void {
         require __DIR__ . '/../views/' . $view .'.php';
+    }
+
+    protected function sendPageMethodNotAllowed(): void
+    {
+        http_response_code(405);
+        $this->view("templates/header", [
+            'title' => "HTTP Method Not Allowed!"
+        ]);
+        $this->view("pages/general/client_method_not_allowed");
+    }
+
+    protected function sendPageNotFound(): void
+    {
+        http_response_code(404);
+        $this->view("templates/header", [
+                'title' => '404 Not Found!'
+        ]);
+        $this->view("pages/general/page_not_found");
+    }
+
+    protected function sendWarningJSON(int $response_code, string $message): void
+    {
+        http_response_code($response_code);
+        echo json_encode([
+            "status"=>"error",
+            "message"=>$message
+        ]);
     }
 }
